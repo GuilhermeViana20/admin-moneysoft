692bfaf4fb2852aae4ec7d1cc01c44b9a7e5047f
Guilherme Viana <guilhermeviana347@gmail.com>
2023-06-22 02:36:23 -0300

    Adicionando validação de token no roteamento

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,35 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import NProgress from 'nprogress'
 import routes from '@/router/routes'
 import { sidebarState } from '@/composables'
+import api from '@/api'
 
 const router = createRouter({
   history: createWebHashHistory('kui-dashboard-vue'),
   routes,
 })
 
-router.beforeEach(() => {
+router.beforeEach(async (to, from, next) => {
   NProgress.start()
+
+  const token = localStorage.getItem('token');
+
+  if (!token && to.path !== '/auth/login') {
+    next('/auth/login');
+  } else if (token && to.path !== '/auth/login') {
+    try {
+      const response = await api.get('/auth/validate-token');
+
+      if (response.status === 200) {
+        next();
+      } else {
+        next('/auth/login');
+      }
+    } catch (e) {
+      next('/auth/login');
+    }
+  } else {
+    next();
+  }
 })
 
 router.afterEach(() => {
