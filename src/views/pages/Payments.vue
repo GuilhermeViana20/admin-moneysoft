e1d3ef50827e0a69063a8193247e3eca180b4115
Guilherme Viana <guilhermeviana347@gmail.com>
2023-06-27 13:02:06 -0300

    Adicionando valores no header da tabela da página pagamentos

--- src/views/pages/Payments.vue
+++ src/views/pages/Payments.vue
@@ -1,17 +1,28 @@
 <script setup>
+import api from '@/api'
 import { ref } from 'vue';
+import { Icon } from '@iconify/vue'
 import PageWrapper from '@/components/PageWrapper.vue';
 import Button from '@/components/Button.vue'
-import { Icon } from '@iconify/vue'
-import api from '@/api'
+import Badge from '@/components/Badge.vue'
 
-const items = ref(null)
 const showTable = ref(false)
+const items = ref(null)
+const pay_paid = ref(null)
+const pay_unpaid = ref(null)
+const receive_paid = ref(null)
+const receive_unpaid = ref(null)
 
 const loadData = async () => {
     try {
         const response = await api.get('/payments');
+
         items.value = response.data.payments;
+        pay_paid.value = response.data.values.pay_paid;
+        pay_unpaid.value = response.data.values.pay_unpaid;
+        receive_paid.value = response.data.values.receive_paid;
+        receive_unpaid.value = response.data.values.receive_unpaid;
+
         showTable.value = true
     } catch (error) {
         console.error(error);
@@ -26,6 +37,20 @@ loadData();
 <template>
   <PageWrapper title="Pagamentos" url="/pages/add/payment">
     <div class="overflow-x-auto">
+
+      <div class='py-4 flex flex-col gap-4 md:flex-row md:items-center md:justify-between'>
+        <div>
+          <h6>Pagamentos:</h6>
+          <span>Realizados: <Badge :title="pay_paid" badge="green" /></span>
+          <span class='px-3'>Pendentes: <Badge :title="pay_unpaid" badge="red" /></span>
+        </div>
+        <div>
+          <h6>Receitas:</h6>
+          <span>Realizados: <Badge :title="receive_paid" badge="green" /></span>
+          <span class='px-3'>Pendentes: <Badge :title="receive_unpaid" badge="red" /></span>
+        </div>
+      </div>
+
       <table class="min-w-full">
         <thead>
           <tr>
@@ -43,24 +68,22 @@ loadData();
             <td class="py-2 px-4">{{ item.amount }}</td>
             <td class="py-2 px-4">{{ item.expiry_date }}</td>
             <td class="py-2 px-4">
-                <Icon v-if="item.status === 'paid' " width='30' icon="mdi:check" v-show="!isDark" aria-hidden="true" :class="iconSizeClasses" />
-                <Icon v-else width='30' icon="mdi:close" v-show="!isDark" aria-hidden="true" :class="iconSizeClasses" />
+                <Icon v-if="item.status === 'paid' " width='30' icon="mdi:check" aria-hidden="true" :class="iconSizeClasses" />
+                <Icon v-else width='30' icon="mdi:close" aria-hidden="true" :class="iconSizeClasses" />
             </td>
             <td class="py-2 px-4">
-                <Icon v-if="item.type === 'pay' " width='30' icon="mdi:bank-transfer-out" v-show="!isDark" aria-hidden="true" :class="iconSizeClasses" />
-                <Icon v-else width='30' icon="mdi:bank-transfer-in" v-show="!isDark" aria-hidden="true" :class="iconSizeClasses" />
+                <Icon v-if="item.type === 'pay' " width='30' icon="mdi:bank-transfer-out" aria-hidden="true" :class="iconSizeClasses" />
+                <Icon v-else width='30' icon="mdi:bank-transfer-in" aria-hidden="true" :class="iconSizeClasses" />
             </td>
             <td>
-              <Button iconOnly variant="secondary" @click="toggleDarkMode()" v-slot="{ iconSizeClasses }"
+              <Button iconOnly variant="secondary" v-slot="{ iconSizeClasses }"
                 class="hidden md:inline-flex mx-1" srText="Toggle dark mode">
-                <Icon icon="mdi:pencil" v-show="!isDark" aria-hidden="true" :class="iconSizeClasses" />
-                <Icon icon="mdi:white-pencil" v-show="isDark" aria-hidden="true" :class="iconSizeClasses" />
+                <Icon icon="mdi:pencil" aria-hidden="true" :class="iconSizeClasses" />
               </Button>
 
-              <Button iconOnly variant="secondary" @click="toggleDarkMode()" v-slot="{ iconSizeClasses }"
+              <Button iconOnly variant="secondary" v-slot="{ iconSizeClasses }"
                 class="hidden md:inline-flex mx-1" srText="Toggle dark mode">
-                <Icon icon="mdi:trash-can-outline" v-show="!isDark" aria-hidden="true" :class="iconSizeClasses" />
-                <Icon icon="mdi:white-trash-can-outline" v-show="isDark" aria-hidden="true" :class="iconSizeClasses" />
+                <Icon icon="mdi:trash-can-outline" aria-hidden="true" :class="iconSizeClasses" />
               </Button>
             </td>
           </tr>
