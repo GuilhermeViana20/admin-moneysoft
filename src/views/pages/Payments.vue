ecac50b787dee0ea9f7e195b1167593304f33247
Guilherme Viana <guilhermeviana347@gmail.com>
2023-06-27 13:14:14 -0300

    Adicionando paginação na página de pagamentos

--- src/views/pages/Payments.vue
+++ src/views/pages/Payments.vue
@@ -5,6 +5,7 @@ import { Icon } from '@iconify/vue'
 import PageWrapper from '@/components/PageWrapper.vue';
 import Button from '@/components/Button.vue'
 import Badge from '@/components/Badge.vue'
+import Pagination from '@/components/Pagination.vue'
 
 const showTable = ref(false)
 const items = ref(null)
@@ -12,16 +13,18 @@ const pay_paid = ref(null)
 const pay_unpaid = ref(null)
 const receive_paid = ref(null)
 const receive_unpaid = ref(null)
+const pagination = ref(null)
 
-const loadData = async () => {
+const loadData = async (page) => {
     try {
-        const response = await api.get('/payments');
+        const response = await api.get('/payments?page=' + page);
 
         items.value = response.data.payments;
         pay_paid.value = response.data.values.pay_paid;
         pay_unpaid.value = response.data.values.pay_unpaid;
         receive_paid.value = response.data.values.receive_paid;
         receive_unpaid.value = response.data.values.receive_unpaid;
+        pagination.value = response.data.pagination;
 
         showTable.value = true
     } catch (error) {
@@ -89,6 +92,9 @@ loadData();
           </tr>
         </tbody>
       </table>
+
+      <Pagination :pagination="pagination" @page-change="loadData" />
+
     </div>
   </PageWrapper>
 </template>
